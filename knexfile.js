08810dad3e2bb44ddab303d2eb7235f461712cf9
Laura Furman <lcfurman11@gmail.com>
2022-08-29 18:38:22 -0400

    update knexfile for tests to pass

--- knexfile.js
+++ knexfile.js
@@ -1,17 +1,43 @@
 const path = require('path')
 require('dotenv').config();
-const { DATABASE_URL } = process.env
+const { DATABASE_URL = "postgres://qvyfxdtg:98KeTuagw35Z5-BjYhYkl1q8nV3kgWWX@salt.db.elephantsql.com/qvyfxdtg" } = process.env
 
 module.exports = {
   development: {
-    client: 'postgresql',
-    connection: "postgres://qvyfxdtg:98KeTuagw35Z5-BjYhYkl1q8nV3kgWWX@salt.db.elephantsql.com/qvyfxdtg",
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
     migrations: {
-      directory: path.join(__dirname, 'src', 'db', 'migrations')
+      directory: path.join(__dirname, "src", "db", "migrations"),
     },
     seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds")
-    }
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
   },
 
+  production: {
+    client: "postgresql",
+    connection: DATABASE_URL,
+    pool: { min: 0, max: 5 },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+  },
+
+  test: {
+    client: "sqlite3",
+    connection: {
+      filename: ":memory:",
+    },
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", "seeds"),
+    },
+    useNullAsDefault: true,
+  },
 };
